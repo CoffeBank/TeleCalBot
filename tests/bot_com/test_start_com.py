71ef8f77bcd48b513dba30db9797bb7ded06ecd2
CoffeBank <ed.yarulin@mail.ru>
2024-05-06 13:52:49 +0300

    test start com

---
+++ tests/bot_com/test_start_com.py
@@ -0,0 +1,59 @@
+import pytest
+from unittest.mock import AsyncMock
+from bot.main_com import start
+from bot.text import HELLO_TEXT
+
+# Mocks
+class MockUser:
+    def __init__(self, id):
+        self.id = id
+
+class MockChat:
+    def __init__(self, id):
+        self.id = id
+
+class MockBot:
+    async def send_message(self, chat_id, text):
+        pass
+
+
+@pytest.mark.asyncio
+async def test_start_function():
+    update_mock = AsyncMock()
+    context_mock = AsyncMock()
+    context_mock.bot = AsyncMock()
+    update_mock.effective_user = MockUser(932872542)
+    update_mock.effective_chat = MockChat(123)
+    update_mock.message.text = "/start"
+
+    await start(update_mock, context_mock)
+
+    context_mock.bot.send_message.assert_called_once_with(
+        chat_id=123,
+        text=HELLO_TEXT)
+
+
+"""
+async def test_name_function():
+    update_mock = AsyncMock()
+    context_mock = AsyncMock()
+    context_mock.bot = AsyncMock()
+    update_mock.effective_user = MockUser(932872542)
+    update_mock.effective_chat = MockChat(123)
+
+    update_mock.message.text = "/name New List"
+    context_mock.args = ["New", "List"]
+
+    await name(update_mock, context_mock)
+
+    context_mock.bot.send_message.assert_called_once_with(
+        chat_id=123,
+        text="Your list will be named <b>New List</b>. You're set!",
+        parse_mode="HTML",
+    )
+"""
+
+"""
+if __name__ == "__main__":
+    pytest.main()
+"""
